736ccb9b368c431bc807a35c70be222a38fe9fa6
Fronikuniu <Fronikuniu@gmail.com>
2024-03-12 21:46:08 +0100

    Update README

--- apps/frontend-e2e/src/example.spec.ts
+++ apps/frontend-e2e/src/example.spec.ts
@@ -5,6 +5,7 @@ test('has title', async ({ page }) => {
 
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain(
-    'Analog. The fullstack Angular meta-framework'
+    `Hello there,
+Welcome frontend 👋`
   );
 });
