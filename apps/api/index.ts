e7e1736ecb0b3d91dc2aae65b15a7259e2f32306
Fronikuniu <Fronikuniu@gmail.com>
2024-03-20 12:30:22 +0100

    Add api deploy configuration

---
+++ apps/api/index.ts
@@ -0,0 +1,3 @@
+import strapi from '@strapi/strapi';
+// @ts-ignore
+strapi(/* {...} */).start();
---
+++ apps/api/src/admin/app.tsx
@@ -0,0 +1,11 @@
+// @ts-ignore
+import ckeditor5Dll from 'ckeditor5/build/ckeditor5-dll.js';
+// @ts-ignore
+import ckeditor5MrkdownDll from '@ckeditor/ckeditor5-markdown-gfm/build/markdown-gfm.js';
+
+export default {
+  config: {},
+  bootstrap(app: any) {
+    console.log(app);
+  },
+};
